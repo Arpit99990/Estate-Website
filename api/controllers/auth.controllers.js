228a672060823955cfb091ce4f25c5d175a3cb48
Interviewbit <arpittiwari621@gmail.com>
2025-11-05 11:05:42 +0530

    .

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -4,6 +4,7 @@ import prisma from "../lib/prisma";
 export const register = async (req,res)=>{
     const {username,email,password} = req.body;
 
+    try{
     // Hash the password
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -16,7 +17,14 @@ export const register = async (req,res)=>{
         },
     });
 
+    console.log(newUser);
+
     res.status(201).json({message: "User created successfully"});
+
+    }catch(err) {
+        console.log(err);
+        res.status(500).json({message: "Failed to create user!"});
+    }
 };
 
 export const login = (req,res)=>{
