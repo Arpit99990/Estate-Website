c2bf2fc1e03fbe04aaf41409fe4f3a6e558b5fd0
Interviewbit <arpittiwari621@gmail.com>
2026-01-14 20:27:20 +0530

    post route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,21 +1,13 @@
 import express from "express";
+import {verifyToken} from "../middleware/verifyToken.js";
+import { addPost, deletePost, getPost, getPosts, updatePost } from "../controllers/post.controller.js";
 
-const postRoute = express.Router();
+const router = express.Router();
 
-postRoute.get("/test", (req,res)=>{
-    console.log("router works!");
-})
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.post("/", verifyToken, addPost);
+router.put("/:id", verifyToken, updatePost);
+router.delete("/:id", verifyToken, deletePost);
 
-postRoute.post("/test", (req,res)=>{
-    console.log("router works!");
-})
-
-postRoute.put("/test", (req,res)=>{
-    console.log("router works!");
-})
-
-postRoute.delete("/test", (req,res)=>{
-    console.log("router works!");
-})
-
-export default postRoute;
+export default router;
